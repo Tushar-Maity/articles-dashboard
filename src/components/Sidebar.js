6dfd67aa281830f9d7f33bdb95dd45368dfd2007
Tushar_Maity <68024552+Tushar-Maity@users.noreply.github.com>
2021-06-19 23:59:42 +0530

    Sidebar contents added

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,35 @@
 import React from 'react'
 
+import { AiOutlineUnorderedList } from 'react-icons/ai'
+import { BsCardHeading } from 'react-icons/bs'
+
 const Sidebar = () => {
     return (
-        <div>
-            <h1>I am the SideBar</h1>
+        <div className="sidebar__Container">
+            <div className="profile">
+                <img src="https://source.unsplash.com/random/40x40" alt="user"/>
+                <div>
+                    <h3>Hi Reader,</h3>
+                    <p>Hers's your New!</p>
+                </div>
+            </div>
+
+            <div className="toggle">
+                <h2>View Toggle</h2>
+                <div className="toggle__Icon">
+                    <div className="card">
+                        <BsCardHeading size="35px"/>
+                    </div>
+                    <div className="list">
+                        <AiOutlineUnorderedList size="35px"/>
+                    </div>
+                </div>
+            </div>
+
+            <div className="feedback">
+                <h2>Have a Feedback</h2>
+                <button>We're Listening</button>
+            </div>
         </div>
     )
 }
