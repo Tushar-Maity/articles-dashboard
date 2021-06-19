e499bbe759e8bb443cb5ddc7b141056871f99470
Tushar_Maity <68024552+Tushar-Maity@users.noreply.github.com>
2021-06-19 23:33:10 +0530

    Http request acieved with build of dashboard c.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Dashboard from './components/Dashboard'
 
 const App = () => {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios.get(`https://api.first.org/data/v1/news`)
+  
+      console.log(result.data.data)
+      console.log(result)
+      setData(result.data.data)
+    }
+  
+    fetchItems()
+  }, [])
+
   return (
     <div>
-       
+       <Dashboard data={data}/>
     </div>
   );
 }
