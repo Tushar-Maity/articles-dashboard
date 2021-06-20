a1ed4d59303dd4516dd41133f851949090de5893
Tushar_Maity <68024552+Tushar-Maity@users.noreply.github.com>
2021-06-20 09:03:52 +0530

    SideBar component Ui finished

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './Dashboard.css'
 
 import Sidebar from './Sidebar'
 import NewsFeed from './NewsFeed'
 
-const Dashboard = () => {
+const Dashboard = ({ data }) => {
+
+    const [cardView, setCardView] = useState(false)
+    const [listView, setListView] = useState(true)
+
     return (
         <div className="container">
-            <Sidebar />
-            <NewsFeed />
+            <Sidebar 
+            cardView={cardView} 
+            setCardView={setCardView} 
+            listView={listView} 
+            setListView={setListView}/>
+
+            <NewsFeed data={data} cardView={cardView} listView={listView}/>
         </div>
     )
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
+import './Sidebar.css'
+
 import { AiOutlineUnorderedList } from 'react-icons/ai'
 import { BsCardHeading } from 'react-icons/bs'
 
-const Sidebar = () => {
+const Sidebar = ({ cardView, setCardView, listView, setListView }) => {
+
+    const CardActive = () => {
+        setCardView(true)
+        setListView(false)
+    }
+
+    const ListActive = () => {
+        setListView(true)
+        setCardView(false)
+    }
+
     return (
         <div className="sidebar__Container">
             <div className="profile">
@@ -17,17 +30,23 @@ const Sidebar = () => {
             <div className="toggle">
                 <h2>View Toggle</h2>
                 <div className="toggle__Icon">
-                    <div className="card">
+                    <div className="card" 
+                    cardView={cardView} 
+                    onClick={CardActive} 
+                    style={{backgroundColor: cardView ? '#96EBC7' : "#D3DAE3"}}>
                         <BsCardHeading size="35px"/>
                     </div>
-                    <div className="list">
+                    <div className="list" 
+                    listView={listView} 
+                    onClick={ListActive} 
+                    style={{backgroundColor: listView ? '#96EBC7' : "#D3DAE3"}}>
                         <AiOutlineUnorderedList size="35px"/>
                     </div>
                 </div>
             </div>
 
             <div className="feedback">
-                <h2>Have a Feedback</h2>
+                <h2>Have a Feedback?</h2>
                 <button>We're Listening</button>
             </div>
         </div>
